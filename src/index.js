266c64dadfc5aa2ee849e037c94e4553b8cbd08f
kuracjusz <witek.kosior@gmail.com>
2017-08-07 00:59:19 +0200

    next session

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyAw-zgzcVRmF6uXiH2rlKl_UJdCGcd1S-0";
 
@@ -25,6 +26,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
